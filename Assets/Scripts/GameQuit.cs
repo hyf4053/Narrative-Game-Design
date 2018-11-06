9ff7f1ebb91bbc9d738ebcf67a164cb2b7fe5e78
hyf4053 <Lanmaodextudio@outlook.com>
2018-11-06 11:48:30 +0800

    加入最基础的菜单并集成了最基础的Inky的功能，API兼容级别从.NET 2.0 subset调整到.NET 2.0

---
+++ Assets/Scripts/GameQuit.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameQuit : MonoBehaviour {
+	public void GameExit(){
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/MainMenuCanvasJump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuCanvasJump : MonoBehaviour {
+	public void NewGame(){
+		StartCoroutine(LoadAsyncScene());
+	}
+
+	IEnumerator LoadAsyncScene(){
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Example");
+		while (!asyncLoad.isDone){
+			yield return null;
+		}
+	}
+
+}
